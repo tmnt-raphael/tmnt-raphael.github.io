1e7a20c9d3566125616a636557edb534b4310784
Ken Luy <Ken@Kens-MacBook-Pro-2.local>
2016-12-04 20:31:14 -0800

    added typing animation

---
+++ js/typer.js
@@ -0,0 +1,16 @@
+$(function(){
+    $(".home").typed({
+        strings: ["Ken Luy"],
+        startDelay: 0,
+        showCursor: true,
+        typeSpeed: 100,
+        onStringTyped: function() {document.getElementsByClassName("typed-cursor")[0].className="empty";}
+    });
+    $(".hack").typed({
+        strings: ["Hacker. Developer. Learner"],
+        startDelay: 2800,
+        showCursor: true,
+        typeSpeed: 100,
+        onStringTyped: function() {document.getElementsByClassName("typed-cursor")[0].className="empty";}
+    });
+});
