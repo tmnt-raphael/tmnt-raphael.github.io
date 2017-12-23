e7eab6dad0001314caa7f4a1dde36fc29c368f50
Ken Luy <Ken@Kens-MacBook-Pro-2.local>
2017-12-23 15:08:44 -0800

    No typing for name.

--- js/typer.js
+++ js/typer.js
@@ -1,14 +1,7 @@
 $(function(){
-    $(".name").typed({
-        strings: ["Ken Luy"],
-        startDelay: 0,
-        showCursor: true,
-        typeSpeed: 100,
-        onStringTyped: function() {document.getElementsByClassName("typed-cursor")[0].className="empty";}
-    });
     $(".skills").typed({
         strings: ["Software Engineer. Programmer. Learner."],
-        startDelay: 2000,
+        startDelay: 1000,
         showCursor: true,
         typeSpeed: 100,
         onStringTyped: function() {document.getElementsByClassName("typed-cursor")[0].className="empty";}
